815b4c996818f993795413131f7f020e04780a2b
evanlipp <lippevan@gmail.com>
2023-10-11 21:31:51 +0300

    refactor app component

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,9 @@
 <template>
-  
-    <button @click="get">get user</button>
-    <button @click="logout">logout</button>
-    <RouterView></RouterView>
-
+    <RouterView/>
 </template>
-
-<script setup>
-import { signOut } from 'firebase/auth'
-import { auth } from '@/firebase'
-import { useRouter } from 'vue-router'
-
-
-
-const get = () => {
-  if (auth.currentUser) {
-  
-    console.log(`user name = ${auth.currentUser.displayName}`)
-    console.log(`user id = ${auth.currentUser.uid}`)
-  } else {
-    console.log(`logged user = false`)
-  }
-
-}
-</script>
 
 <style lang="scss">
 #app {
-  width: 100%;
   height: 100%;
 }
 </style>
