18c0f56c941e287ca77880c6f9fc5debeb3fb204
evanlipp <lippevan@gmail.com>
2023-10-12 00:46:27 +0300

    fix MainView styles

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,20 +1,18 @@
 <template>
   <div class="main">
     <div class="navigation">
-      <button @click="add">add</button>
-      <button @click="get">get</button>
       <h1 class="logo">blog,</h1>
       <router-link class="navigation__link" to="profile">My profile</router-link>
-      <router-link class="navigation__link" to="posts" >My posts</router-link>
-      <MyButton class="navigation__link" @click="isVisible = !isVisible">New post</MyButton>
+      <router-link class="navigation__link" to="posts">My posts</router-link>
+      <MyButton class="button navigation__link" @click="isModalVisible = !isModalVisible">New post</MyButton>
       <router-link class="navigation__link" to="auth" @click="logOut">Quit</router-link>
     </div>
     <div class="content">
-      <router-view></router-view>
+      <router-view />
     </div>
   </div>
-  <MyModal :visibility="isVisible" @hideModal="isVisible = !isVisible">
-    <NewPostForm @hideModal="isVisible = !isVisible"/>
+  <MyModal :visibility="isModalVisible" @hideModal="isModalVisible = !isModalVisible">
+    <NewPostForm @hideModal="isModalVisible = !isModalVisible" />
   </MyModal>
 </template>
 
@@ -23,43 +21,31 @@ import MyButton from '@/components/UI/MyButton';
 import MyModal from '@/components/UI/MyModal';
 import NewPostForm from '@/components/NewPostForm';
 import { signOut } from 'firebase/auth';
-import { auth } from '@/firebase';
+import { auth } from '@/firebase/init';
 import { ref } from 'vue';
+import { useStore } from 'vuex';
 
-const isVisible = ref(false)
+const store = useStore();
+
+const isModalVisible = ref(false);
 
 const logOut = () => {
   signOut(auth)
-}
-
-import { dataBase } from '@/firebase';
-import { collection, addDoc, doc, setDoc, updateDoc, serverTimestamp, getDoc, getDocs, onSnapshot } from "firebase/firestore"; 
-
-
-
-const get = async () => {
-  const docref = await getDoc(doc(dataBase, "users", auth.currentUser.uid))
-  const res = docref.data()
-  console.log(Object.values(res))
-}
-
-
-
-const add = async () => {
-  await setDoc(doc(dataBase, 'users', auth.currentUser.uid ), {first: {title: 'bh', desc: 'bhb'}, second: {title: 'bbhbh', desc: 'bhoob'}})
-  
-}
-
-
+};
 </script>
 
 <style lang="scss" scoped>
 .main {
-  max-width: 1400px;
-  padding: 100px 200px 0 200px;
+  max-width: 1240px;
   height: 100%;
+  padding: 50px 20px;
+  margin: auto;
   display: flex;
-  gap: 40px;
+  gap: 60px;
+
+  @media (max-width: 520px) {
+    gap: 10px;
+  }
 }
 
 .navigation {
@@ -67,15 +53,20 @@ const add = async () => {
   flex-direction: column;
   gap: 30px;
 
+  @media (max-width: 520px) {
+    gap: 15px;
+  }
+
   &__link {
     font-size: 20px;
-    color: inherit;
     text-align: left;
     letter-spacing: 2px;
     white-space: nowrap;
-    border: none;
-    background-color: transparent;
     transition: transform 100ms;
+
+    @media (max-width: 520px) {
+      font-size: 12px;
+    }
 
     &:hover {
       transform: scale(1.1);
@@ -92,13 +83,17 @@ const add = async () => {
 }
 
 .content {
-  background-color: $bg-secondary;
   width: 100%;
   border-radius: 7px;
   box-shadow: 0px 0px 53px -22px rgba(1, 11, 0, 0.22);
+  background-color: $bg-secondary;
 }
 
 .logo {
   font: $font-logo;
+
+  @media (max-width: 520px) {
+    font-size: 20px;
+  }
 }
 </style>
